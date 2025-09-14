27d2156930706c5fcfccd419b6a4e3965267964a
Yassin-Ibrahim5 <148269339+Yassin-Ibrahim5@users.noreply.github.com>
2025-09-14 22:06:05 +0300

    Styled the register page

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -7,6 +7,7 @@ import {useRouter} from "next/navigation";
 import axios from "axios";
 import {Loader} from "lucide-react";
 import toast from "react-hot-toast";
+import ShowHidePassword from "@/components/input-comps/ShowHidePassword";
 
 export default function RegisterPage() {
     interface Inputs {
@@ -41,34 +42,46 @@ export default function RegisterPage() {
     }
 
     return (
-        <div>
-            <div className="w-1/2 mx-auto my-10">
-                <h2 className="text-3xl tracking-tighter font-bold">Register</h2>
+        <div className={`my-30 font-[Poppins]`}>
+            <div className="w-1/2 mx-auto">
+                <h2 className="text-3xl mb-5 text-start font-bold uppercase font-[Poppins] text-[#222222]">Register</h2>
                 {errorMessage && <p className="text-red-700 text-center text-lg">{errorMessage}</p>}
                 <form onSubmit={handleSubmit(onSubmit)}>
-                    <Input {...register("name", {required: "Name is Required"})} className="p-5 my-5" type="text"
+                    <Input {...register("name", {required: "Name is Required"})}
+                           className="p-5 my-5 focus-visible:ring-[#717FE080]" type="text"
                            placeholder="Name"/>
                     {errors.name && <p className="text-red-700">{errors.name.message}</p>}
-                    <Input {...register("email", {required: "Email is Required"})} className="p-5 my-5" type="email"
+
+                    <Input {...register("email", {required: "Email is Required"})}
+                           className="p-5 my-5 focus-visible:ring-[#717FE080]" type="email"
                            placeholder="Email"/>
                     {errors.email && <p className="text-red-700">{errors.email.message}</p>}
-                    <Input {...register("password", {required: "Password is Required"})} className="p-5 my-5"
-                           type="password" placeholder="Password"/>
+
+                    <div className="relative passwordInput">
+                        <ShowHidePassword {...register("password", {required: "Password is Required"})}
+                                          placeholder="Password"/>
+                    </div>
                     {errors.password && <p className="text-red-700">{errors.password.message}</p>}
-                    <Input {...register("rePassword", {required: "Confirm Password is Required"})} className="p-5 my-5"
-                           type="password"
-                           placeholder="RePassword"/>
+
+                    <div className="relative rePasswordInput">
+                        <ShowHidePassword {...register("rePassword", {required: "Confirm Password is Required"})}
+                                          placeholder="RePassword"/>
+                    </div>
                     {errors.rePassword && <p className="text-red-700">{errors.rePassword.message}</p>}
-                    <Input {...register("phone", {required: "Phone Number is Required"})} className="p-5 my-5"
+
+                    <Input {...register("phone", {required: "Phone Number is Required"})}
+                           className="p-5 my-5 focus-visible:ring-[#717FE080]"
                            type="text" placeholder="Phone Number"/>
                     {errors.phone && <p className="text-red-700">{errors.phone.message}</p>}
+
                     <div className="flex items-center gap-5">
-                        <Button type="submit" disabled={loading} className="px-10 py-5 my-5 cursor-pointer">
+                        <Button type="submit" disabled={loading}
+                                className={`px-10 py-5 cursor-pointer my-5 bg-black rounded-[22px] hover:bg-[#717FE0] hover:text-white border-1 transition-all duration-400 border-[#e6e6e6] text-white uppercase font-[Poppins]`}>
                             {loading ? <Loader className={`animate-spin`} size={20}/> : "Register"}
                         </Button>
                         <p className="text-center">
                             Already have an account? <a href="/login"
-                                                        className="text-blue-500 hover:underline">Login</a>
+                                                        className="text-[#717FE0] hover:underline">Login</a>
                         </p>
                     </div>
                 </form>
