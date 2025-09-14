9e15483eec7f796bf1d0adacf2edb04c050a602b
Yassin-Ibrahim5 <148269339+Yassin-Ibrahim5@users.noreply.github.com>
2025-09-14 03:08:38 +0300

    Styled wishlist page

--- src/app/wishlist/page.tsx
+++ src/app/wishlist/page.tsx
@@ -2,7 +2,7 @@ import WishlistTable from "@/components/wish-comps/WishlistTable";
 
 export default function WishlistPage() {
     return (
-        <div>
+        <div className={`flex justify-center items-center h-screen`}>
             <WishlistTable/>
         </div>
     );
--- src/components/wish-comps/WishlistTable.tsx
+++ src/components/wish-comps/WishlistTable.tsx
@@ -11,6 +11,7 @@ import toast from "react-hot-toast";
 import {removeFromWishlist} from "@/actions/wishlist.action";
 import {HeartOff} from "lucide-react";
 import LoadingPage from "@/app/wishlist/loading";
+import {Button} from "@/components/ui/button";
 
 export default function WishlistTable() {
     const {wishlist, fetchWishlist, loading} = useWishlist();
@@ -42,58 +43,63 @@ export default function WishlistTable() {
         <>
             {loading ? <LoadingPage/> :
                 (wishlist?.count !== 0 ?
-                (<div className={`w-3/4 mx-auto`}>
-                    <Table>
-                        <TableHeader>
-                            <TableRow>
-                                <TableHead className={`p-6 text-center`}>Product</TableHead>
-                                <TableHead className={`p-6 text-center`}>Price</TableHead>
-                                <TableHead className={`p-6 text-center`}>Action</TableHead>
-                            </TableRow>
-                        </TableHeader>
-                        <TableBody className={`text-center`}>
-                            {wishlist?.data?.map((product) => (
-                                <TableRow key={product._id}>
-                                    <TableCell className={`p-6 text-center`}>
-                                        <div className="flex items-center justify-center gap-4">
-                                            <div className="relative">
-                                                <Badge onClick={() => {
-                                                    handleRemoveFromWishlist(product._id);
-                                                }} className={`absolute top-[-8] left-[-16px] cursor-pointer`}>X</Badge>
-                                                <Image src={product.imageCover} alt={product.title} width={60}
-                                                       height={60}/>
-                                            </div>
-                                            <Link href={`/products/${product._id}`}>
-                                                <h2>{product.title.split(" ").slice(0, 2).join(" ")}</h2>
-                                            </Link>
-                                        </div>
-                                    </TableCell>
-                                    <TableCell className="p-6 text-center">{product.price} EGP</TableCell>
-                                    <TableCell className="p-6 text-center">
-                                        <button onClick={() => {
-                                            handleAddToCart(product._id);
-                                        }}
-                                                className={`font-medium hover:bg-black hover:border-black hover:text-white transition-all duration-200 border-1 px-5 py-2 rounded-md cursor-pointer border-slate-200`}>
-                                            Add to Cart
-                                        </button>
-                                    </TableCell>
-                                </TableRow>
-                            ))}
-                        </TableBody>
-                    </Table>
-                </div>) :
-                <div className={`w-1/2 mx-auto flex flex-col items-center justify-center gap-5 my-10`}>
-                    <div className={`flex items-center justify-center rounded-full bg-slate-100 p-10`}>
-                        <HeartOff className={`text-6xl text-black`} size={62}/>
-                    </div>
-                    <h2 className={`text-3xl font-bold`}>Your wishlist is empty!</h2>
-                    <p className={`text-md w-[75%] text-center text-slate-500`}>Found something you like? Tap on the heart shaped icon next
-                        to the item to add it to your wishlist! All your saved items will appear here.</p>
-                    <Link href={`/`}
-                          className={`px-8 py-4 hover:bg-black hover:text-white cursor-pointer rounded-lg border-1 transition-all duration-300 border-black`}>
-                        Continue Shopping
-                    </Link>
-                </div>
+                        (<div className={`w-3/4 mx-auto`}>
+                            <Table className={`w-full border-2`}>
+                                <TableHeader>
+                                    <TableRow>
+                                        <TableHead
+                                            className={`p-6 text-center uppercase font-[Poppins] font-bold text-[#555]`}>Product</TableHead>
+                                        <TableHead
+                                            className={`p-6 text-center uppercase font-[Poppins] font-bold text-[#555]`}>Price</TableHead>
+                                        <TableHead
+                                            className={`p-6 text-center uppercase font-[Poppins] font-bold text-[#555]`}>Action</TableHead>
+                                    </TableRow>
+                                </TableHeader>
+                                <TableBody className={`text-center`}>
+                                    {wishlist?.data?.map((product) => (
+                                        <TableRow key={product._id} className={`hover:bg-white`}>
+                                            <TableCell className={`p-6 text-center`}>
+                                                <div className="flex items-center justify-center gap-4">
+                                                    <div className="relative">
+                                                        <Badge onClick={() => {
+                                                            handleRemoveFromWishlist(product._id).then();
+                                                        }}
+                                                               className={`absolute top-[-8] left-[-16px] cursor-pointer hover:bg-[#717FE0] transition-all duration-300`}>X</Badge>
+                                                        <Image src={product.imageCover} alt={product.title} width={60}
+                                                               height={60}/>
+                                                    </div>
+                                                    <Link href={`/products/${product._id}`}>
+                                                        <h2 className={`text-[#555] font-[Poppins] hover:text-[#717FE0] transition-all duration-300`}>{product.title.split(" ").slice(0, 2).join(" ")}</h2>
+                                                    </Link>
+                                                </div>
+                                            </TableCell>
+                                            <TableCell className="p-6 text-center text-[#555] font-[Poppins]">{product.price} EGP</TableCell>
+                                            <TableCell className="p-6 text-center text-[#555] font-[Poppins]">
+                                                <Button onClick={() => {
+                                                    handleAddToCart(product._id).then();
+                                                }}
+                                                        className={`px-8 py-5 cursor-pointer bg-black rounded-[22px] hover:bg-[#717FE0] hover:text-white border-1 transition-all duration-400 border-[#e6e6e6] text-white uppercase font-[Poppins]`}>
+                                                    Add to Cart
+                                                </Button>
+                                            </TableCell>
+                                        </TableRow>
+                                    ))}
+                                </TableBody>
+                            </Table>
+                        </div>) :
+                        <div className={`w-1/2 mx-auto flex flex-col items-center justify-center gap-5 my-10`}>
+                            <div className={`flex items-center justify-center rounded-full bg-slate-100 p-10`}>
+                                <HeartOff className={`text-6xl text-black`} size={62}/>
+                            </div>
+                            <h2 className={`text-3xl font-bold`}>Your wishlist is empty!</h2>
+                            <p className={`text-md w-[75%] text-center text-slate-500`}>Found something you like? Tap on
+                                the heart shaped icon next
+                                to the item to add it to your wishlist! All your saved items will appear here.</p>
+                            <Link href={`/`}
+                                  className={`px-8 py-4 hover:bg-black hover:text-white cursor-pointer rounded-lg border-1 transition-all duration-300 border-black`}>
+                                Continue Shopping
+                            </Link>
+                        </div>
                 )
             }
         </>
