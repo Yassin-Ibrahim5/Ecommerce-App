0a684572d989e5c6e9afc0d99d78e09b6822d156
Yassin-Ibrahim5 <148269339+Yassin-Ibrahim5@users.noreply.github.com>
2025-09-14 22:10:21 +0300

    Styled the forgot password page.

--- src/app/(auth)/forgot-password/page.tsx
+++ src/app/(auth)/forgot-password/page.tsx
@@ -38,18 +38,22 @@ export default function ForgotPassword() {
     }
 
     return (
-        <div>
-            <div className="w-1/2 mx-auto my-10">
-                <h2 className="text-3xl tracking-tighter font-bold">Recover your account</h2>
-                <p className="text-gray-500">You can request a password reset below. We will send a security code to the
+        <div className="my-30 font-[Poppins]">
+            <div className="w-1/2 mx-auto">
+                <h2 className="text-3xl mb-3 text-start font-bold uppercase font-[Poppins] text-[#222222]">Recover your
+                    Account</h2>
+                <p className="text-gray-500 ">You can request a password reset below. We will send a security code to
+                    the
                     email address, please make sure it is correct.</p>
-                {errorMessage && <p className="text-red-700 text-center text-lg">{errorMessage}</p>}
+                {errorMessage && <p className="text-red-500 text-lg">{errorMessage}</p>}
                 <form onSubmit={handleSubmit(onSubmit)}>
-                    <Input {...register("email", {required: "Email is Required"})} className="p-5 my-5" type="email"
+                    <Input {...register("email", {required: "Email is Required"})}
+                           className="p-5 my-5 focus-visible:ring-[#717FE080]" type="email"
                            placeholder="Email"/>
-                    {errors.email && <p className="text-red-700">{errors.email.message}</p>}
+                    {errors.email && <p className="text-red-500">{errors.email.message}</p>}
                     <div className="flex items-center gap-5">
-                        <Button type="submit" disabled={loading} className="px-10 py-5 my-5 cursor-pointer">
+                        <Button type="submit" disabled={loading}
+                                className={`px-10 py-5 cursor-pointer my-5 bg-black rounded-[22px] hover:bg-[#717FE0] hover:text-white border-1 transition-all duration-400 border-[#e6e6e6] text-white uppercase font-[Poppins]`}>
                             {loading ? <Loader className={`animate-spin`} size={20}/> : "Send"}
                         </Button>
                     </div>
