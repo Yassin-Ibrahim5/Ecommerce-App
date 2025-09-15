c9df507dcbf031c9e6e4ba2f18a371617096d9cb
Yassin-Ibrahim5 <148269339+Yassin-Ibrahim5@users.noreply.github.com>
2025-09-15 03:59:22 +0300

    Use shadcn's carousel instead of SwiperSlide

--- src/components/sliders-comps/CatSliderComp.tsx
+++ src/components/sliders-comps/CatSliderComp.tsx
@@ -1,42 +1,60 @@
 "use client";
-"use client"
 import React from 'react';
-import {Swiper, SwiperSlide} from 'swiper/react';
-import {Navigation} from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/pagination';
-import Image from "next/image";
 import {Category} from "@/app/types/category.model";
-import Link from "next/link";
+import {Carousel, CarouselContent, CarouselItem} from "@/components/ui/carousel";
+import Autoplay from "embla-carousel-autoplay";
+import Image from "next/image";
+import {Card, CardContent} from "@/components/ui/card";
+
 
 function CatSliderComp({category}: { category: Category[] }) {
     return (
-        <div className={`container mx-auto`}>
-            <Swiper
-                slidesPerView={4}
-                spaceBetween={4}
-                navigation={true}
-                loop={true}
-                modules={[Navigation,]}
-                className="mySwiper mt-10">
-                {category.map((cat) =>
-                    <SwiperSlide key={cat._id}>
-                        <div className={`relative h-[450px] w-full group`}>
-                            <Image src={cat.image} alt={`slider-images`} fill
-                                   sizes="(max-width:768px) 100vw (max-width:1280px) 50vw, 25" priority
-                                   loading={"eager"}
-                                   className={`object-cover`}/>
-                            <div className={`absolute top-[0] left-[0] w-full h-full bg-[#6775d6cc] opacity-0 group-hover:opacity-100 transition-all duration-500`}>
-                            </div>
-                            <div className={`absolute -bottom-[500px] w-full h-[450px] text-white group-hover:-bottom-[390px] text-center transition-all duration-600`}>
-                                <Link href={"/products"} className={`text-2xl font-[Poppins] hover:underline text-center font-semibold`}>Shop Now</Link>
-                            </div>
-                        </div>
-                        <p className={`text-2xl text-center font-[Poppins] font-semibold`}>{cat.name}</p>
-                    </SwiperSlide>
-                )}
-            </Swiper>
-        </div>
+        <>
+            <div className="mx-auto">
+                <Carousel className="w-full max-w-[100vw]" opts={
+                    {
+                        align: "start",
+                        loop: true,
+                    }
+                } plugins={
+                    [
+                        Autoplay({
+                            delay: 3500
+                        }),
+                    ]
+                }>
+                    <CarouselContent>
+                        {category.map((cat) => (
+                            <CarouselItem className={`relative basis-full md:basis-1/2 xl:basis-1/4`} key={cat._id}>
+                                <Card>
+                                    <CardContent>
+                                        <div className={`relative h-[450px] group`}>
+                                            <Image src={cat.image} alt={cat.name} fill
+                                                   sizes="(max-width:768px) 100vw (max-width:1280px) 50vw, 25" priority
+                                                   loading={"eager"}
+                                                   className={`object-cover`}/>
+                                            {/*<div*/}
+                                            {/*    className={`absolute top-[0] left-[0] w-full h-full bg-[#6775d6cc] opacity-0 group-hover:opacity-100 transition-all duration-500`}>*/}
+                                            {/*</div>*/}
+                                            {/*<div*/}
+                                            {/*    className={`absolute -bottom-[500px] w-full h-[450px] text-white group-hover:-bottom-[390px] text-center transition-all duration-600`}>*/}
+                                            {/*    <Link href={"/products"}*/}
+                                            {/*          className={`text-2xl font-[Poppins] hover:underline text-center font-semibold`}>Shop*/}
+                                            {/*        Now</Link>*/}
+                                            {/*</div>*/}
+                                        </div>
+                                        <p className={`text-2xl text-center font-[Poppins] font-semibold`}>{cat.name}</p>
+                                    </CardContent>
+                                </Card>
+                            </CarouselItem>
+                        ))}
+                    </CarouselContent>
+                </Carousel>
+            </div>
+        </>
+
     );
 }
 
