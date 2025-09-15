61124f3a5ac7b16710f84914b1e2e41cb32e2c3d
Yassin-Ibrahim5 <148269339+Yassin-Ibrahim5@users.noreply.github.com>
2025-09-15 15:05:02 +0300

    Styled the orders' page

--- src/components/orders-comps/OrdersTable.tsx
+++ src/components/orders-comps/OrdersTable.tsx
@@ -4,7 +4,8 @@ import {useOrders} from "@/app/context/OrdersContext";
 import {Table, TableBody, TableCell, TableHead, TableHeader, TableRow} from "@/components/ui/table";
 import Link from "next/link";
 import {Logs} from "lucide-react";
-import LoadingPage from "@/app/allorders/loading";
+import LoadingPage from "@/app/orders/loading";
+import {Button} from "@/components/ui/button";
 
 export default function OrdersTable() {
     const {orders, loading} = useOrders();
@@ -13,20 +14,25 @@ export default function OrdersTable() {
             {loading ? <LoadingPage/> :
                 (orders?.length !== 0 ?
                         (<div className={`w-3/4 mx-auto`}>
-                            <Table>
+                            <h2 className="text-4xl mb-5 text-start font-bold uppercase font-[Poppins] text-[#222222]">Orders</h2>
+                            <Table className={`w-full border-2 font-[Poppins]`}>
                                 <TableHeader>
                                     <TableRow>
-                                        <TableHead className={`p-6 text-center`}>Order Number</TableHead>
-                                        <TableHead className={`p-6 text-center`}>Status</TableHead>
-                                        <TableHead className={`p-6 text-center`}>Total Price</TableHead>
-                                        <TableHead className={`p-6 text-center`}>Action</TableHead>
+                                        <TableHead className={`p-6 text-center uppercase font-bold text-[#555]`}>Order
+                                            Number</TableHead>
+                                        <TableHead
+                                            className={`p-6 text-center uppercase font-bold text-[#555]`}>Status</TableHead>
+                                        <TableHead className={`p-6 text-center uppercase font-bold text-[#555]`}>Total
+                                            Price</TableHead>
+                                        <TableHead
+                                            className={`p-6 text-center uppercase font-bold text-[#555]`}>Action</TableHead>
                                     </TableRow>
                                 </TableHeader>
-                                <TableBody className={`text-center`}>
+                                <TableBody>
                                     {orders?.map((order) => (
-                                        <TableRow key={order._id}>
-                                            <TableCell className={`p-6 text-center text-gray-500`}>
-                                                {order.id}
+                                        <TableRow key={order._id} className={`hover:bg-white`}>
+                                            <TableCell className={`p-6 text-center`}>
+                                                <h2 className={`text-[#555] font-bold font-[Poppins] hover:text-[#717FE0] transition-all duration-300`}>{order.id}</h2>
                                             </TableCell>
                                             <TableCell
                                                 className={`p-6 text-center flex items-center justify-center gap-2`}>
@@ -34,33 +40,38 @@ export default function OrdersTable() {
                                                     <p className={`bg-green-500 rounded-lg py-2 px-2 w-[75px] text-white`}>Paid</p> :
                                                     <p className={`bg-orange-500 rounded-lg w-[75px] py-2 px-2 text-white`}>Pending</p>}
                                             </TableCell>
-                                            <TableCell className={`p-6 text-center`}>
+                                            <TableCell className={`p-6 text-center text-[#555]`}>
                                                 {order.totalOrderPrice} EGP
                                             </TableCell>
                                             <TableCell className={`p-6 text-center`}>
-                                                <Link href={`/allorders`}
-                                                      className={`font-medium hover:bg-black hover:border-black hover:text-white transition-all duration-200 border-1 px-5 py-2 rounded-md cursor-pointer border-slate-200`}>
-                                                    View Order
-                                                </Link>
+                                                <Button
+                                                    className={`px-8 py-5 cursor-pointer bg-black rounded-[22px] hover:bg-[#717FE0] border-1 transition-all duration-400 border-[#e6e6e6] text-white uppercase font-[Poppins]`}>
+                                                    <Link href={`/orders`}>
+                                                        View Order
+                                                    </Link>
+                                                </Button>
                                             </TableCell>
                                         </TableRow>
                                     ))}
                                 </TableBody>
                             </Table>
                         </div>) :
-                        <div className={`w-1/2 mx-auto flex flex-col items-center justify-center gap-5 my-10`}>
-                            <div className={`flex items-center justify-center rounded-full bg-slate-100 p-10`}>
-                                <Logs className={`text-6xl text-black`} size={62}/>
+                        <div className={`w-1/2 mx-auto flex flex-col items-center justify-center gap-5 font-[Poppins]`}>
+                            <div
+                                className={`flex items-center justify-center rounded-full bg-slate-100 p-10 group hover:bg-[#717FE0] transition-all duration-400`}>
+                                <Logs
+                                    className={`text-6xl text-black group-hover:text-white transition-all duration-400`}
+                                    size={62}/>
                             </div>
                             <h2 className={`text-3xl font-bold`}>Your orders are empty!</h2>
                             <p className={`text-md w-[75%] text-center text-slate-500`}>Found something you like? Tap on
                                 the
-                                cart shaped icon next
-                                to the item to add it to your cart and complete your first order! All your orders will
+                                add to cart button on the item to add it to your cart and complete your first order! All
+                                your orders will
                                 appear
                                 here.</p>
                             <Link href={`/cart`}
-                                  className={`px-8 py-4 hover:bg-black hover:text-white cursor-pointer rounded-lg border-1 transition-all duration-300 border-black`}>
+                                  className={`px-8 py-4 cursor-pointer my-5 bg-black rounded-[22px] hover:bg-[#717FE0] border-1 transition-all duration-400 border-[#e6e6e6] text-white uppercase font-[Poppins]`}>
                                 Go to Cart
                             </Link>
                         </div>
