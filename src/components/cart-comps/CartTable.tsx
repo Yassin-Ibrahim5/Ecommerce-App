a4430ffe21b5331ada93b41cae58c4bfab639735
Yassin-Ibrahim5 <148269339+Yassin-Ibrahim5@users.noreply.github.com>
2025-09-17 00:42:10 +0300

    Made the cart page responsive

--- src/components/cart-comps/CartTable.tsx
+++ src/components/cart-comps/CartTable.tsx
@@ -40,9 +40,11 @@ export default function CartTable() {
         <>
             {loading ? <LoadingPage/> :
                 (cartDetails?.numOfCartItems !== 0 ?
-                        (<div className={`w-3/4 mx-auto`}>
-                            <h2 className="text-4xl mb-5 text-start font-bold uppercase font-[Poppins] text-[#222222]">Cart</h2>
-                            <Table className={`w-full border-2`}>
+                        (<div className={`w-full lg:w-3/4 mx-auto px-4 lg:px-0`}>
+                            <h2 className="text-2xl md:text-4xl mb-5 md:text-start font-bold uppercase font-[Poppins] text-[#222222]">Cart</h2>
+
+                            {/*Desktop View Table*/}
+                            <Table className={`w-full border-2 hidden md:table`}>
                                 <TableHeader>
                                     <TableRow>
                                         <TableHead
@@ -124,20 +126,78 @@ export default function CartTable() {
                                     </TableRow>
                                 </TableBody>
                             </Table>
+
+                            {/*Mobile View Table*/}
+                            <div className="md:hidden font-[Poppins]">
+                                {cartDetails?.data.products.map((product) => (
+                                    <div key={product._id}
+                                         className={`bg-white border-2 rounded-lg p-4 p-4 mb-4 shadow-sm`}>
+                                        <div className="flex items-center justify-between mb-4">
+                                            <div className="flex items-center gap-4">
+                                                <Image src={product.product.imageCover} alt={product.product.title}
+                                                       width={80} height={80} className={`rounded-md`}/>
+                                                <Link href={`/products/${product.product._id}`}>
+                                                    <h3 className="text-base font-bold text-[#555] hover:text-[#717FE0]">
+                                                        {product.product.title.split(" ").slice(0, 3).join(" ")}
+                                                    </h3>
+                                                </Link>
+                                            </div>
+                                            <button onClick={() => {
+                                                handleRemoveFromCart(product.product._id).then()
+                                            }}
+                                                    className={`text-gray-500 hover:text-red-500 transition-all duration-400 cursor-pointer`}>
+                                                <Trash2 size={24}/>
+                                            </button>
+                                        </div>
+                                        <div className="grid grid-cols-2 gap-2 text-sm text-gray-700">
+                                            <div className="font-semibold">Price:</div>
+                                            <div>{product.price} EGP</div>
+                                            <div className="flex items-center justify-start">
+                                                <button
+                                                    onClick={() => handleUpdateCart(product.product._id, product.count - 1)}
+                                                    className="w-8 h-8 flex items-center justify-center border hover:bg-[#717FE0] hover:text-white transition-all duration-400 cursor-pointer">-
+                                                </button>
+                                                <span
+                                                    className="w-8 h-8 flex items-center justify-center border-t border-b bg-gray-100">{product.count}</span>
+                                                <button
+                                                    onClick={() => handleUpdateCart(product.product._id, product.count + 1)}
+                                                    className="w-8 h-8 flex items-center justify-center border hover:bg-[#717FE0] hover:text-white transition-all duration-400 cursor-pointer">+
+                                                </button>
+                                            </div>
+                                            <div className={`flex`}>
+
+                                                <div className="font-semibold">Subtotal: &nbsp;</div>
+                                                <div>{product.price * product.count} EGP</div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                ))}
+                                <div className="bg-white p-4 rounded-lg shadow-sm text-right font-bold mt-4">
+                                    <h4 className="text-xl">Total: {cartDetails?.data.totalCartPrice} EGP</h4>
+                                    <Button
+                                        className="mt-4 w-full px-10 py-5 cursor-pointer bg-black rounded-[22px] hover:bg-[#717FE0] hover:text-white border-1 transition-all duration-400 border-[#e6e6e6] text-white uppercase font-[Poppins]">
+                                        <Link href={"/checkout"}>
+                                            Checkout
+                                        </Link>
+                                    </Button>
+                                </div>
+                            </div>
                         </div>) :
-                        <div className={`w-1/2 mx-auto flex flex-col items-center justify-center gap-5 font-[Poppins]`}>
+                        <div
+                            className={`w-11/12 md:w-1/2 mx-auto flex flex-col items-center justify-center gap-5 font-[Poppins]`}>
                             <div
                                 className={`flex items-center justify-center rounded-full bg-slate-100 p-10 group hover:bg-[#717FE0] transition-all duration-400`}>
                                 <ShoppingBag
                                     className={`text-6xl text-black group-hover:text-white transition-all duration-400`}
                                     size={62}/>
                             </div>
-                            <h2 className={`text-3xl font-bold`}>Your cart is empty!</h2>
-                            <p className={`text-md w-[75%] text-center text-[#888]`}>Found something you like? Tap on
+                            <h2 className={`text-2xl md:text-3xl font-bold text-center`}>Your cart is empty!</h2>
+                            <p className={`text-sm md:text-md w-full text-center text-[#888]`}>Found something you like?
+                                Tap on
                                 the
                                 add to cart button on the item to add it to your cart!</p>
-                            <Link href={`/`}
-                                  className={`px-8 py-4 cursor-pointer my-5 bg-black rounded-[22px] hover:bg-[#717FE0] border-1 transition-all duration-400 border-[#e6e6e6] text-white uppercase font-[Poppins]`}>
+                            <Link href={`/products`}
+                                  className={`px-6 py-3 md:px-8 md:py-4 cursor-pointer my-5 bg-black rounded-[22px] hover:bg-[#717FE0] border-1 transition-all duration-400 border-[#e6e6e6] text-white uppercase font-[Poppins]`}>
                                 Continue Shopping
                             </Link>
                         </div>
