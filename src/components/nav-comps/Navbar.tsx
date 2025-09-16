46e6c93b49c0db39359739b591539980acae04d3
Yassin-Ibrahim5 <148269339+Yassin-Ibrahim5@users.noreply.github.com>
2025-09-16 23:12:48 +0300

    Minor style changes

--- src/components/nav-comps/Navbar.tsx
+++ src/components/nav-comps/Navbar.tsx
@@ -288,7 +288,7 @@ export default function Navbar() {
                                 <Menu className={`hover:text-[#717fe0] transition-all duration-400`}/>
                             </Button>
                         </SheetTrigger>
-                        <SheetContent className={`w-[250px] sm:w-[300px] flex font-[Poppins] p-5`} side={`left`}>
+                        <SheetContent className={`w-[250px] sm:w-[300px] flex font-[Poppins] p-5`} side={`right`}>
                             <SheetHeader className={`flex flex-row justify-between items-start gap-2`}>
                                 <SheetTitle className={`text-[#333] leading-6 text-[18px] font-bold uppercase`}>Navigation</SheetTitle>
                                 <SheetClose className={`top-0 right-0 md:top-4 md:right-4`} asChild>
