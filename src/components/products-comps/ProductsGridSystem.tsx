87251a3d6222b0292fc1f9531373c5f4b73b9bf7
Yassin-Ibrahim5 <148269339+Yassin-Ibrahim5@users.noreply.github.com>
2025-09-16 23:08:41 +0300

    Minor style changes

--- src/components/products-comps/ProductsGridSystem.tsx
+++ src/components/products-comps/ProductsGridSystem.tsx
@@ -6,8 +6,8 @@ import {ScrollArea, ScrollBar} from "@/components/ui/scroll-area";
 
 export default function ProductsGridSystem({products}: { products: { data: Product[], } }) {
     return (
-        <div className={`w-full md:w-[85%] mt-10 md:mt-15 mx-auto`}>
-            <h2 className="text-3xl md:text-4xl mb-5 text-start font-bold uppercase font-[Poppins] text-[#222222] px-4 md:px-10">Products
+        <div className={`w-full md:w-[85%] mt-10 md:mt-15 mx-auto py-10 px-4 md:px-10`}>
+            <h2 className="text-3xl md:text-4xl mb-5 text-start font-bold uppercase font-[Poppins] text-[#222222] px-4">Products
                 Overview</h2>
             <Tabs className={`font-[Poppins] transition-all duration-400`} defaultValue={"all-products"}>
                 <ScrollArea className={`md:hidden bg-transparent`}>
@@ -46,26 +46,26 @@ export default function ProductsGridSystem({products}: { products: { data: Produ
                                  className={`text-[#666] hover:text-[#333] text-[16px] data-[state=active]:text-[#333] data-[state=active]:border-none data-[state=active]:shadow-none data-[state=active]:underline transition-all duration-400 cursor-pointer`}>
                         Electronics</TabsTrigger>
                 </TabsList>
-                <TabsContent value={"all-products"} className={`transition-all duration-400 p-4 md:p-10`}>
+                <TabsContent value={"all-products"} className={`transition-all duration-400 p-4 md:p-0`}>
                     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5">
                         {products?.data?.map((product) => <ProductCard key={product._id} product={product}/>)}
                     </div>
                 </TabsContent>
-                <TabsContent value={"women's-fashion"} className={`transition-all duration-400`}>
+                <TabsContent value={"women's-fashion"} className={`transition-all duration-400 p-4 md:p-0`}>
                     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5">
                         {products.data.filter((product) => product.category.name === "Women's Fashion").map((product) => (
                             <ProductCard key={product._id} product={product}/>
                         ))}
                     </div>
                 </TabsContent>
-                <TabsContent value={"mens-fashion"} className={`transition-all duration-400`}>
+                <TabsContent value={"mens-fashion"} className={`transition-all duration-400 p-4 md:p-0`}>
                     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5">
                         {products.data.filter((product) => product.category.name === "Men's Fashion").map((product) => (
                             <ProductCard key={product._id} product={product}/>
                         ))}
                     </div>
                 </TabsContent>
-                <TabsContent value={"electronics"} className={`transition-all duration-400`}>
+                <TabsContent value={"electronics"} className={`transition-all duration-400 p-4 md:p-0`}>
                     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5">
                         {products.data.filter((product) => product.category.name === "Electronics").map((product) => (
                             <ProductCard key={product._id} product={product}/>
