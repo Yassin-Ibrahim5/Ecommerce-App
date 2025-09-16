441400839dc6f359a745ea6b25a9f844ea53fb4b
Yassin-Ibrahim5 <148269339+Yassin-Ibrahim5@users.noreply.github.com>
2025-09-16 14:10:57 +0300

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,8 @@ import {NextResponse} from 'next/server'
 export function middleware(request: NextRequest) {
 
     const token = request.cookies.get("next-auth.session-token");
-    if (!token) {
+    const otherToken = request.cookies.get("__Secure-next-auth.session-token");
+    if (!token && !otherToken) {
         return NextResponse.redirect(new URL('/login', request.url));
     }
     return NextResponse.next();
