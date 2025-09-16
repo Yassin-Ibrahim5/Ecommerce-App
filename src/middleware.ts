ad88a4356bdc1e7f3a5589d5007094c76dc29654
Yassin-Ibrahim5 <148269339+Yassin-Ibrahim5@users.noreply.github.com>
2025-09-16 13:49:12 +0300

    Changed middleware.ts urls

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,5 @@
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
+import type {NextRequest} from 'next/server'
+import {NextResponse} from 'next/server'
 
 export function middleware(request: NextRequest) {
 
@@ -11,5 +11,5 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-    matcher: ["/cart", "/wishlist", "/checkout", "/orders"],
+    matcher: ["/", "/cart", "/wishlist", "/checkout", "/orders", "/products", "/brands", "/categories"],
 }
