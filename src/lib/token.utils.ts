89ee14ca9ccf6ca1ce58b138910a6e59b1fa4654
Yassin-Ibrahim5 <148269339+Yassin-Ibrahim5@users.noreply.github.com>
2025-09-16 18:15:24 +0300

    Optimizations for deployment

--- src/lib/token.utils.ts
+++ src/lib/token.utils.ts
@@ -4,7 +4,12 @@ import {decode} from "next-auth/jwt";
 
 export async function getUserToken() {
     const encodedToken = (await cookies()).get("next-auth.session-token")?.value;
+    const otherToken = (await cookies()).get("__Secure-next-auth.session-token")?.value;
 
+    if (!encodedToken) {
+        const preTokenOther = await decode({ token: otherToken, secret: process.env.AUTH_SECRET !});
+        return preTokenOther?.token;
+    }
     const preToken = await decode({ token: encodedToken, secret: process.env.AUTH_SECRET !});
     return preToken?.token;
 }
