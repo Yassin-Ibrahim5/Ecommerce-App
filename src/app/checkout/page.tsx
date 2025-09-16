8a8cafb2a5a49041152d90477432d499d3679771
Yassin-Ibrahim5 <148269339+Yassin-Ibrahim5@users.noreply.github.com>
2025-09-17 02:26:44 +0300

    Made the checkout page responsive

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -71,44 +71,43 @@ export default function CheckoutPage() {
     }
 
     return (
-        <div className={`my-30 font-[Poppins]`}>
-            <div className="w-1/2 mx-auto">
-                <h2 className="text-3xl mb-5 text-start font-bold uppercase font-[Poppins] text-[#222222]">Checkout</h2>
-                {errorMessage && <p className="text-red-500 text-center text-lg">{errorMessage}</p>}
-                <p className="text-lg tracking-tighter text-[#555]">Please fill in the form below to complete your
+        <div className={`my-30 font-[Poppins] px-4 sm:px-6 lg:px-8`}>
+            <div className="w-full md:w-3/4 lg:w-1/2 mx-auto">
+                <h2 className="text-2xl md:text-3xl mb-5 md:text-start font-bold uppercase font-[Poppins] text-[#222222]">Checkout</h2>
+                {errorMessage && <p className="text-red-500 text-center text-sm md:text-lg">{errorMessage}</p>}
+                <p className="text-sm md:text-lg text-[#555] text-start">Please fill in the form below to complete your
                     order.</p>
-                <form onSubmit={handleSubmit(onSubmit)}>
+                <form onSubmit={handleSubmit(onSubmit)} className={`mt-5`}>
                     <Input {...register("details", {required: "Shipping Details are Required"})}
-                           className="p-5 my-5 focus-visible:ring-[#717FE080]"
+                           className="p-3 md:p-5 my-3 md:my-5 focus-visible:ring-[#717FE080]"
                            type="text"
                            placeholder="Shipping Details"/>
-                    {errors.details && <p className="text-red-500">{errors.details.message}</p>}
+                    {errors.details && <p className="text-red-500 text-sm">{errors.details.message}</p>}
                     <Input {...register("phone", {required: "Phone Number is Required"})}
-                           className="p-5 my-5 focus-visible:ring-[#717FE080]"
+                           className="p-3 md:p-5 my-3 md:my-5 focus-visible:ring-[#717FE080]"
                            type="text" placeholder="Phone Number"/>
-                    {errors.phone && <p className="text-red-500">{errors.phone.message}</p>}
+                    {errors.phone && <p className="text-red-500 text-sm">{errors.phone.message}</p>}
                     <Input {...register("city", {required: "City is Required"})}
-                           className="p-5 my-5 focus-visible:ring-[#717FE080]"
+                           className="p-3 md:p-5 my-3 md:my-5 focus-visible:ring-[#717FE080]"
                            type="text" placeholder="City"/>
-                    {errors.city && <p className="text-red-500">{errors.city.message}</p>}
-                    <div className="flex items-center gap-5">
-                        <Button type={"submit"} disabled={loading}
-                                className={`px-10 py-5 cursor-pointer my-5 bg-black rounded-[22px] hover:bg-[#717FE0] hover:text-white border-1 transition-all duration-400 border-[#e6e6e6] text-white uppercase font-[Poppins]`}>
-                        {loading ? <Loader className={`animate-spin`} size={20}/> : "Place Order"}
-                        </Button>
+                    {errors.city && <p className="text-red-500 text-sm">{errors.city.message}</p>}
+                    <div className="flex flex-col sm:flex-row items-center justify-between sm:gap-5 mt-5">
                         <RadioGroup onValueChange={(value) => {
                             setPaymentMethod(value as PaymentMethod);
-                        }} className={`flex`} defaultValue="cash">
-                            <div className="flex items-center space-x-2 group">
-                                <RadioGroupItem value="cash"
-                                                id="cash"/>
-                                <Label htmlFor="cash" className={`group-hover:text-[#717FE0] transition-all duration-400`}>Cash Payment</Label>
+                        }} className={`flex gap-4 mb-4 sm:mb-0`} defaultValue="cash">
+                            <div className="flex items-center space-x-2 group cursor-pointer">
+                                <RadioGroupItem value="cash" id="cash" className={`cursor-pointer`}/>
+                                <Label htmlFor="cash" className={`group-hover:text-[#717FE0] cursor-pointer transition-all duration-400 text-sm`}>Cash Payment</Label>
                             </div>
-                            <div className="flex items-center space-x-2 group">
-                                <RadioGroupItem value="card" id="card"/>
-                                <Label htmlFor="card" className={`group-hover:text-[#717FE0] transition-all duration-400`}>Card Payment</Label>
+                            <div className="flex items-center space-x-2 group cursor-pointer">
+                                <RadioGroupItem value="card" id="card" className={`cursor-pointer`}/>
+                                <Label htmlFor="card" className={`group-hover:text-[#717FE0] cursor-pointer transition-all duration-400 text-sm`}>Card Payment</Label>
                             </div>
                         </RadioGroup>
+                        <Button type={"submit"} disabled={loading}
+                                className={`w-full sm:w-auto px-6 py-3 md:px-10 md:my-5 cursor-pointer bg-black rounded-[22px] hover:bg-[#717FE0] hover:text-white border-1 transition-all duration-400 border-[#e6e6e6] text-white uppercase font-[Poppins]`}>
+                            {loading ? <Loader className={`animate-spin`} size={20}/> : "Place Order"}
+                        </Button>
                     </div>
                 </form>
             </div>
